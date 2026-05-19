33e181392e8c19fa2ce28b58736af0e27eadcb59
Ave-Misumi <misumi429@qq.com>
2026-05-19 11:46:49 +0800

    修正 remap32fLanczos4C1 函数中的舍入模式，确保输出值的准确性

--- hal/riscv-rvv/src/imgproc/warp.cpp
+++ hal/riscv-rvv/src/imgproc/warp.cpp
@@ -385,7 +385,7 @@ static inline int remap32fLanczos4C1(int start, int end, const uchar *src_data, 
 
                 auto fixed_weight = [&](typename RVV_SameLen<float, helper>::VecType a, typename RVV_SameLen<float, helper>::VecType b) {
                     auto prod = __riscv_vfmul(a, b, vl);
-                    return __riscv_vnclip(__riscv_vfcvt_x(__riscv_vfmul(prod, INTER_REMAP_COEF_SCALE, vl), vl), 0, __RISCV_VXRM_RNU, vl);
+                    return __riscv_vnclip(__riscv_vfcvt_x(__riscv_vfmul(prod, INTER_REMAP_COEF_SCALE, vl), vl), 0, __RISCV_VXRM_RNE, vl);
                 };
 
                 intertab(imx);
@@ -425,7 +425,7 @@ static inline int remap32fLanczos4C1(int start, int end, const uchar *src_data, 
                 auto k7 = row_sum(c7, iy7);
                 auto sum = __riscv_vadd(__riscv_vadd(__riscv_vadd(__riscv_vadd(k0, k1, vl), k2, vl), k3, vl), __riscv_vadd(__riscv_vadd(__riscv_vadd(k4, k5, vl), k6, vl), k7, vl), vl);
                 sum = __riscv_vmax(sum, 0, vl);
-                helper::vstore(reinterpret_cast<T*>(dst_data + i * dst_step) + j, __riscv_vnclipu(__riscv_vnclipu(__riscv_vreinterpret_v_i32m2_u32m2(sum), INTER_REMAP_COEF_BITS, __RISCV_VXRM_RNU, vl), 0, __RISCV_VXRM_RNU, vl), vl);
+                helper::vstore(reinterpret_cast<T*>(dst_data + i * dst_step) + j, __riscv_vnclipu(__riscv_vnclipu(__riscv_vreinterpret_v_i32m2_u32m2(sum), INTER_REMAP_COEF_BITS, __RISCV_VXRM_RNE, vl), 0, __RISCV_VXRM_RNE, vl), vl);
             }
             else
             {
